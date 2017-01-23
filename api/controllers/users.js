a5aaadec7221bb1d90b2d0857ac0baf087329772
Mikey Lee <mikey1384@gmail.com>
2017-01-23 20:24:14 +0900

    api test

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -2,6 +2,7 @@ const User = require('../models/user');
 
 module.exports = {
   signup(req, res, next) {
+    console.log("came in")
     const userProps = req.body;
     User.create(userProps)
       .then(user => res.send(user))
